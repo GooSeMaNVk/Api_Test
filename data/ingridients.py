fb0a8d353e6b8b94004d15797148701cd43635a2
GooSeMaNVk <134262661+GooSeMaNVk@users.noreply.github.com>
2023-10-12 17:37:26 +0700

    Initial commit

---
+++ data/ingridients.py
@@ -0,0 +1,15 @@
+class Hash:
+    def pass_hash(self):
+        pass_hash = {
+       "ingredients": [
+      "61c0c5a71d1f82001bdaaa6d",
+       "61c0c5a71d1f82001bdaaa70",
+       "61c0c5a71d1f82001bdaaa72",
+          "61c0c5a71d1f82001bdaaa76",
+       ]
+   }
+        return pass_hash
+
+    def false_hash(self):
+        false_hash = {"ingredients": ["61c0c5a71d1f82001bdaSaa6d"]}
+        return false_hash
---
+++ data/link.py
@@ -0,0 +1,19 @@
+class BaseUrl():
+    def authorization(self):
+        link = "https://stellarburgers.nomoreparties.site/api/auth/login"
+        return link
+    def registration(self):
+        link = "https://stellarburgers.nomoreparties.site/api/auth/register"
+        return link
+    def user_change(self):
+        link = "https://stellarburgers.nomoreparties.site/api/auth/user"
+        return link
+    def ingredients(self):
+        link = "https://stellarburgers.nomoreparties.site/api/ingredients"
+        return link
+    def orders(self):
+        link = "https://stellarburgers.nomoreparties.site/api/orders"
+        return link
+    def auth_orders(self):
+        link = "https://stellarburgers.nomoreparties.site/api/orders"
+        return link
